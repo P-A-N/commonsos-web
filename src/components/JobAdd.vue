6f7ee8a1408c3fc765b25b0d8f4bb3f63d696c22
Kirill Klenski, Patrick Abner <kirill@codeborne.com, patrick@codeborne.com>
2018-03-02 15:17:56 +0200

    154757344 change price -> points

--- src/components/JobAdd.vue
+++ src/components/JobAdd.vue
@@ -5,28 +5,30 @@
         <label for="title">Title</label>
         <input type="text" class="form-control" id="title" name="title"
                v-model="job.title" v-validate="'required'">
-        <p class="alert alert-danger" v-show="errors.has('title')">{{errors.first('title')}}</p>
+        <p class="text-danger" v-show="errors.has('title')">{{errors.first('title')}}</p>
       </div>
 
       <div class="form-group">
         <label for="description">Description</label>
         <input type="text" class="form-control" id="description" name="description"
                v-model="job.description" v-validate="'required'">
-        <p class="alert alert-danger" v-show="errors.has('description')">{{errors.first('description')}}</p>
+        <p class="text-danger" v-show="errors.has('description')">{{errors.first('description')}}</p>
       </div>
 
       <div class="form-group">
-        <label for="price">Price</label>
-        <input type="text" class="form-control" id="price" name="price"
-               v-model="job.price" v-validate="'required|decimal'">
-        <p class="alert alert-danger" v-show="errors.has('price')">{{errors.first('price')}}</p>
+        <label for="points">Points</label>
+        <div class="input-group">
+          <input type="text" class="form-control" id="points" name="points"
+                 v-model="job.points" v-validate="'required|decimal'">
+        </div>
+        <p class="text-danger" v-show="errors.has('points')">{{errors.first('points')}}</p>
       </div>
 
       <div class="form-group">
         <label for="location">Location</label>
         <input type="text" class="form-control" id="location" name="location"
                v-model="job.location" v-validate="'required'">
-        <p class="alert alert-danger" v-show="errors.has('location')">{{errors.first('location')}}</p>
+        <p class="text-danger" v-show="errors.has('location')">{{errors.first('location')}}</p>
       </div>
 
       <button type="submit" class="btn btn-primary">Add</button>
@@ -39,7 +41,7 @@
     name: 'JobAdd',
     data() {
       return {
-        job: {title: '', description: ''},
+        job: {},
       }
     },
     methods: {
