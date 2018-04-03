8c6f0d8383c0f3ce8eba9669df46c0a6843a9e99
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-04-03 14:41:14 +0300

    156369118 convert create ad view to material design

--- src/components/AdCreate.vue
+++ src/components/AdCreate.vue
@@ -1,8 +1,6 @@
 <template>
 
-  <v-dialog fullscreen
-            v-model="showDialog"
-            transition="dialog-bottom-transition"
+  <v-dialog fullscreen v-model="showDialog" transition="dialog-bottom-transition"
             :overlay="false"
             scrollable>
 
@@ -14,46 +12,32 @@
         </v-btn>
         <v-toolbar-title>New advertisement</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn dark flat @click="createAd()">Save</v-btn>
-        </v-toolbar-items>
       </v-toolbar>
 
-      <v-card-text>
-        <form v-on:submit.prevent="createAd">
-          <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title"
-                   v-model="ad.title" v-validate="'required'">
-            <p class="text-danger" v-show="errors.has('title')">{{errors.first('title')}}</p>
-          </div>
-
-          <div class="form-group">
-            <label for="description">Description</label>
-            <input type="text" class="form-control" id="description" name="description"
-                   v-model="ad.description" v-validate="'required'">
-            <p class="text-danger" v-show="errors.has('description')">{{errors.first('description')}}</p>
-          </div>
-
-          <div class="form-group">
-            <label for="reward">Reward</label>
-            <div class="input-group">
-              <input type="text" class="form-control" id="reward" name="reward"
-                     v-model="ad.points" v-validate="'required|decimal'">
-            </div>
-            <p class="text-danger" v-show="errors.has('reward')">{{errors.first('reward')}}</p>
-          </div>
-
-          <div class="form-group">
-            <label for="location">Location</label>
-            <input type="text" class="form-control" id="location" name="location"
-                   v-model="ad.location" v-validate="'required'">
-            <p class="text-danger" v-show="errors.has('location')">{{errors.first('location')}}</p>
-          </div>
-
-          <button type="submit" class="btn btn-primary">Add</button>
-        </form>
-      </v-card-text>
+      <form v-on:submit.prevent="createAd">
+        <v-card-text>
+          <v-text-field v-model="ad.title" label="Title" type="text"
+                        :error-messages="errors.collect('title')"
+                        v-validate="'required'"
+                        data-vv-name="title"/>
+          <v-text-field v-model="ad.description" label="Description" type="text"
+                        :error-messages="errors.collect('description')"
+                        v-validate="'required'"
+                        data-vv-name="description"/>
+          <v-text-field v-model="ad.points" label="Reward" type="text"
+                        :error-messages="errors.collect('reward')"
+                        v-validate="'required|decimal'"
+                        data-vv-name="reward"/>
+          <v-text-field v-model="ad.location" label="Location" type="text"
+                        :error-messages="errors.collect('location')"
+                        v-validate="'required'"
+                        data-vv-name="location"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn right type="submit">Add</v-btn>
+        </v-card-actions>
+      </form>
     </v-card>
   </v-dialog>
 </template>
@@ -66,8 +50,11 @@
     data() {
       return {
         ad: {},
-        showDialog: true
+        showDialog: false
       }
+    },
+    created() {
+      setTimeout(() => this.showDialog = true, 0)
     },
     methods: {
       createAd() {
