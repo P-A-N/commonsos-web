864f941219d56ca254a4e60c392f11749f2c8df2
Erkki Teedla <erkki@codeborne.com>
2018-04-10 09:54:47 +0300

    User should be redirected to wallet by default, this also resolves bottom navigation first item being visualised as selected.

--- src/router.js
+++ src/router.js
@@ -10,16 +10,15 @@ Vue.use(Router)
 
 let router = new Router({
   routes: [
-    {path: '/', redirect: '/transactions'},
+    {path: '/', redirect: '/wallet'},
     {path: '/wallet', component: TransactionList},
     {path: '/community/ads', component: Community},
     {path: '/community/ads/create', component: Community},
     {path: '/community/my-ads', component: Community},
     {path: '/messages', component: Messages},
     {path: '/login', component: Login},
-    {path: '/agreements/:id', component: AgreementDetails, props: true},
-    {path: '/transactions', component: TransactionList},
+    {path: '/agreements/:id', component: AgreementDetails, props: true}
   ]
-})
+});
 
 export default router
