1651d8b064cb7b8fd4ef2816286a42f8f8bccf4b
Erkki Teedla, Tõnis Aruste <erkki@codeborne.com, tonis@codeborne.com>
2018-04-11 15:11:01 +0300

    156567509 update mock data with user ids instead of username; add user full name support

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -18,9 +18,9 @@
     computed: {
       imageSrc() {
         let images = {
-          worker: 'https://image.jimcdn.com/app/cms/image/transf/none/path/s09a03e3ad80f8a02/image/i788e42d25ed4115e/version/1493969515/image.jpg',
-          elderly1: 'https://i.pinimg.com/originals/df/5c/70/df5c70b3b4895c4d9424de3845771182.jpg',
-          elderly2: 'https://qph.fs.quoracdn.net/main-qimg-42b85e5f162e21ce346da83e8fa569bd-c'
+          '0': 'https://image.jimcdn.com/app/cms/image/transf/none/path/s09a03e3ad80f8a02/image/i788e42d25ed4115e/version/1493969515/image.jpg',
+          '1': 'https://i.pinimg.com/originals/df/5c/70/df5c70b3b4895c4d9424de3845771182.jpg',
+          '2': 'https://qph.fs.quoracdn.net/main-qimg-42b85e5f162e21ce346da83e8fa569bd-c'
         }
         return images[this.userId] || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPlkwhBse_JCK37_0WA3m_PHUpFncOVLM0s0c4cCqpV27UteuJ';
       }
