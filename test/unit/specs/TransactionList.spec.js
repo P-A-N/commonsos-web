788b349c1d465376e632561632080d74bb20a9d9
Erkki Teedla, Tõnis Aruste <erkki@codeborne.com, tonis@codeborne.com>
2018-04-11 15:54:23 +0300

    156567509 fix tests

--- test/unit/specs/TransactionList.spec.js
+++ test/unit/specs/TransactionList.spec.js
@@ -16,8 +16,8 @@ describe('TransactionList.vue', () => {
   it('should display user transactions', (done) => {
     gateway.get.and.returnValue(Promise.resolve({
       data: [
-        {createdAt: '2018-01-30T22:51:00', remitterId: 'user1', beneficiaryId: 'user2', amount: 1.11},
-        {createdAt: '2018-02-24T11:22:33', remitterId: 'user2', beneficiaryId: 'user1', amount: 2.22}
+        {createdAt: '2018-01-30T22:51:00', remitter: {id: 'user1', fullName: 'name1'}, beneficiary: {id: 'user2', fullName: 'name2'}, amount: 1.11},
+        {createdAt: '2018-02-24T11:22:33', remitter: {id: 'user2', fullName: 'name2'}, beneficiary: {id: 'user1', fullName: 'name1'}, amount: 2.22}
       ]
     }))
     spyOn(userService, 'user').and.returnValue({id: 'user1'})
@@ -28,11 +28,11 @@ describe('TransactionList.vue', () => {
       let transactions = wrapper.findAll('.transaction')
 
       expect(transactions.at(0).text()).toContain('2 months ago')
-      expect(transactions.at(0).text()).toContain('from user1 to user2')
+      expect(transactions.at(0).text()).toContain('from name1 to name2')
       expect(transactions.at(0).text()).toContain('-1.11')
 
       expect(transactions.at(1).text()).toContain('23 days ago')
-      expect(transactions.at(1).text()).toContain('from user2 to user1')
+      expect(transactions.at(1).text()).toContain('from name2 to name1')
       expect(transactions.at(1).text()).toContain('+2.22')
 
       expect(gateway.get).toHaveBeenCalledWith('transactions')
