5bef1ca39cf07adb510a73b915b698cca0e8560c
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-04-13 11:48:45 +0300

    clean code

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,19 @@
     </v-content>
 
     <v-bottom-nav fixed :value="true" app>
-      <v-btn :to="'/wallet'">
+      <v-btn to="/wallet">
         <span>Wallet</span>
         <v-icon>account_balance_wallet</v-icon>
       </v-btn>
-      <v-btn :to="'/community'">
+      <v-btn to="/community">
         <span>Community</span>
         <v-icon>people</v-icon>
       </v-btn>
-      <v-btn :to="'/messages'">
+      <v-btn to="/messages">
         <span>Messages</span>
         <v-icon>message</v-icon>
       </v-btn>
-      <v-btn :to="'/profile'">
+      <v-btn to="/profile">
         <span>Profile</span>
         <v-icon>person</v-icon>
       </v-btn>
@@ -42,6 +42,3 @@
     }
   }
 </script>
-
-<style>
-</style>
