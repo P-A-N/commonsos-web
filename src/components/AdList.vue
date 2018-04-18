0c70d375cae7f5596f9cd912bdff7efe01bdde2e
Erkki Teedla <erkki@codeborne.com>
2018-04-18 15:44:01 +0300

    Remove redundant markup.

--- src/components/AdList.vue
+++ src/components/AdList.vue
@@ -67,33 +67,6 @@
     </v-container>
   </div>
 
-  <!--<v-expansion-panel v-if="ads.length" popout>-->
-    <!--<v-expansion-panel-content v-for="(ad, index) in ads" :key="index" hide-actions class="ad">-->
-      <!--<v-layout align-center row slot="header">-->
-        <!--<v-flex xs3>-->
-          <!--<avatar :userId="ad.createdBy"/>-->
-        <!--</v-flex>-->
-        <!--<v-flex>-->
-          <!--<div>{{ad.title}}</div>-->
-        <!--</v-flex>-->
-        <!--<v-flex xs2 class="body-2 text-xs-right">-->
-          <!--{{ad.points}}-->
-        <!--</v-flex>-->
-      <!--</v-layout>-->
-      <!--<v-card>-->
-        <!--<v-card-text>-->
-          <!--<div>{{ad.description}}</div>-->
-          <!--<v-chip>{{ad.location}}</v-chip>-->
-        <!--</v-card-text>-->
-        <!--<v-card-actions>-->
-          <!--<v-btn v-if="ad.acceptable" color="blue" @click="messageForAd(ad)" flat>-->
-            <!--Send Message-->
-          <!--</v-btn>-->
-        <!--</v-card-actions>-->
-      <!--</v-card>-->
-    <!--</v-expansion-panel-content>-->
-  <!--</v-expansion-panel>-->
-
 </template>
 
 <script>
