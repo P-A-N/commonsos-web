b0c3e6449cbbb81bd17f6475dc747ede0c0d10f5
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-04-19 12:30:01 +0300

    fix test

--- test/unit/specs/OtherUserProfile.spec.js
+++ test/unit/specs/OtherUserProfile.spec.js
@@ -7,60 +7,51 @@ import gateway from '@/gateway'
 describe('OtherUserProfile.vue', () => {
 
   let router
-  let userDataOnServer
 
   beforeEach(() => {
     router = new VueRouter()
-    userDataOnServer = {fullName: 'other user full name', balance: 10}
-    spyOn(gateway, 'get').and.returnValue(Promise.resolve({data: userDataOnServer}))
   })
 
-  it('should display other user profile', (done) => {
-    spyOn(userService, 'user').and.returnValue({admin: false})
+  it('should display other user profile to admin user', (done) => {
+    spyOn(userService, 'user').and.returnValue({admin: true})
+    spyOn(gateway, 'get').and.returnValue(Promise.resolve({data: {balance: 10, fullName: 'other user full name'}}))
 
     let wrapper = mount(OtherUserProfile, {router, propsData: {userId: 'otheruserid'}})
 
     setTimeout(() => {
-      expect(wrapper.find('.top-up').element).toBeUndefined()
       expect(wrapper.text()).toContain('other user full name')
-      expect(wrapper.text()).toContain('Balance 10')
+      expect(wrapper.text()).toContain('10')
+      expect(wrapper.find('.top-up').element).toBeDefined()
       expect(gateway.get).toHaveBeenCalledWith('/users/otheruserid')
       done()
     }, 0)
   })
 
-  it('should not display balance if not given', (done) => {
+  it('should display other user profile to regular user', (done) => {
     spyOn(userService, 'user').and.returnValue({admin: false})
-    userDataOnServer.balance = null
+    spyOn(gateway, 'get').and.returnValue(Promise.resolve({data: {fullName: 'other user full name'}}))
 
     let wrapper = mount(OtherUserProfile, {router, propsData: {userId: 'otheruserid'}})
 
     setTimeout(() => {
       expect(wrapper.text()).toContain('other user full name')
-      expect(wrapper.text()).not.toContain('Balance')
+      expect(wrapper.find('.top-up').element).toBeUndefined()
       done()
     }, 0)
   })
 
-  it('should display topup button to admin', (done) => {
-    spyOn(userService, 'user').and.returnValue({admin: true})
-
-    let wrapper = shallow(OtherUserProfile, {router})
-
-    setTimeout(() => {
-      expect(wrapper.find('.top-up').element).toBeDefined()
-      done()
-    }, 0)
-  })
 
   it('should reload user balance after modal topup is done', (done) => {
     spyOn(userService, 'user').and.returnValue({admin: true})
+    spyOn(gateway, 'get').and.returnValues(
+        Promise.resolve({data: {balance: 10, fullName: 'other user full name'}}),
+        Promise.resolve({data: {balance: 20, fullName: 'other user full name'}})
+      )
     let wrapper = shallow(OtherUserProfile, {router})
 
     setTimeout(() => {
       expect(wrapper.vm.otherUser.balance).toBe(10)
 
-      userDataOnServer.balance = 20
       wrapper.vm.paymentDone()
 
       setTimeout(() => {
