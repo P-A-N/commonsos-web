4d68965b44e797155d547f89b30069ad086cf219
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-04-25 11:33:50 +0300

    156537616 tune message send input

--- src/components/MessageThread.vue
+++ src/components/MessageThread.vue
@@ -7,31 +7,34 @@
     </app-toolbar>
     <v-list three-line>
       <v-subheader>{{ thread.title }}</v-subheader>
-      <template v-for="message in messages">
+      <template v-for="(message, index) in messages">
         <v-list-tile avatar>
           <v-list-tile-avatar v-if="message.createdBy">
             <img :src="message.createdBy.avatarUrl">
           </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-sub-title v-html="message.text"></v-list-tile-sub-title>
-            <v-list-tile-sub-title v-if="message.createdAt" class="grey--text caption">{{message.createdAt | moment('from')}}</v-list-tile-sub-title>
+            <v-list-tile-sub-title v-if="message.createdAt" class="grey--text caption">{{message.createdAt |
+              moment('from')}}
+            </v-list-tile-sub-title>
             <v-progress-linear v-else :indeterminate="true"></v-progress-linear>
           </v-list-tile-content>
         </v-list-tile>
-        <v-divider></v-divider>
+        <v-divider v-if="index != messages.length-1"></v-divider>
       </template>
     </v-list>
 
-    <v-container>
-      <v-layout row wrap>
-        <v-flex>
-          <form @submit.prevent="sendMessage()">
-            <v-text-field v-model="messageText" :label="$t('MessageThread.message')" type="text" auto-grow multi-line/>
-            <v-btn type="submit" color="primary">{{$t('MessageThread.send')}}</v-btn>
-          </form>
-        </v-flex>
-      </v-layout>
-    </v-container>
+
+    <v-card color="grey lighten-4" class="mt-1 mb-4" flat>
+      <v-card-text style="padding: 0">
+              <form @submit.prevent="sendMessage()">
+                <v-text-field v-model="messageText" :label="$t('MessageThread.message')"
+                              type="text" flat solo rows="2" multi-line/>
+                  <v-btn type="submit" small primary absolute bottom right fab><v-icon>send</v-icon>
+                  </v-btn>
+              </form>
+      </v-card-text>
+    </v-card>
     <div ref="messageInput"></div>
   </div>
 </template>
@@ -63,6 +66,7 @@
         })
       },
       sendMessage() {
+        if (this.messageText === "") return
         let newMessage = {text: this.messageText, createdAt: null}
         this.messages.push(newMessage)
         gateway.post(`/message-threads/${this.threadId}/messages`, {threadId: this.threadId, text: this.messageText})
