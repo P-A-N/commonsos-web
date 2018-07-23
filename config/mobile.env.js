920cfcb924c24d69b9e8a411d397a350e38d6fe0
Kirill Klenski <kirill.klenski@gmail.com>
2018-07-23 15:34:08 +0300

    switch to use prod api url

--- config/mobile.env.js
+++ config/mobile.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  API_BASE_URL: '"https://app.test.commons.love/api"'
+  API_BASE_URL: '"https://app.commons.love/api"'
 })
