83188004d993372d395a34e758eeb30d42578cb9
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-07-25 15:02:09 +0300

    159296329 fix to show correct last message author

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -19,7 +19,7 @@
             </v-list-tile-title>
             <v-list-tile-sub-title style="line-height: 1.3;">
               <span v-if="thread.lastMessage">
-                <span class="text--primary">{{thread.parties[0].fullName}}</span> &ndash; {{thread.lastMessage.text}}
+                <span class="text--primary">{{userById(thread, thread.lastMessage.createdBy).fullName}}</span> &ndash; {{thread.lastMessage.text}}
               </span>
               <span v-else class="caption grey--text">{{$t('Messages.noMessagesInThread')}}</span>
             </v-list-tile-sub-title>
@@ -62,7 +62,12 @@
     created() {
       messagePoller.checkForUnreadThreads()
       gateway.get('/message-threads').then(r => this.messageThreads = r.data)
-    }
+    },
+    methods: {
+      userById(thread, userId) {
+        return (userId === thread.creator.id) ? thread.creator : thread.parties.find(user => user.id === userId)
+      }
+    },
   }
 </script>
 
--- test/unit/specs/Messages.spec.js
+++ test/unit/specs/Messages.spec.js
@@ -6,8 +6,8 @@ import messagePoller from '@/services/MessagePoller'
 describe('Messages.vue', () => {
 
   let threads = [
-    {id: '1', title: 'Thread one', parties: [{id: '22'}], lastMessage: null},
-    {id: '2', title: 'Thread two', parties: [{id: '22'}], lastMessage: {text: 'last message for thread two', createdAt: '2015-12-22T12:33:44Z'}}
+    {id: '1', title: 'Thread one', parties: [{id: '22'}], lastMessage: null, creator: {}},
+    {id: '2', title: 'Thread two', parties: [{id: '22', fullName: 'John Doe'}], lastMessage: {createdBy: '22', text: 'last message for thread two', createdAt: '2015-12-22T12:33:44Z'}, creator: {}}
   ]
 
   it('should display existing message theads', (done) => {
@@ -20,6 +20,7 @@ describe('Messages.vue', () => {
       expect(wrapper.findAll('.thread').at(0).text()).toContain('Thread one')
       expect(wrapper.findAll('.thread').at(1).text()).toContain('Thread two')
       expect(wrapper.findAll('.thread').at(1).text()).toContain('an hour ago')
+      expect(wrapper.findAll('.thread').at(1).text()).toContain('John Doe')
       expect(wrapper.findAll('.thread').at(1).text()).toContain('last message for thread two')
       expect(gateway.get).toHaveBeenCalledWith('/message-threads')
       expect(messagePoller.checkForUnreadThreads).toHaveBeenCalled()
